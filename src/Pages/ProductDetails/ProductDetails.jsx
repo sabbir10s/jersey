aea3765cefe5080c83677f2a060ca5508974890d
sabbir10s <sabbirahmed1023@gmail.com>
2023-12-28 18:35:20 +0600

    add more product details

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -6,8 +6,8 @@ import AddToCard from "../../Components/AddToCard";
 const ProductDetails = () => {
   const { productId } = useParams();
   const [products, setProducts] = useState([]);
-  const [userName, setUserName] = useState("emon");
-  const [number, setNumber] = useState("30");
+  const [jerseyName, setJerseyName] = useState("emon");
+  const [jerseyNumber, setJerseyNumber] = useState("30");
   const apiUrl =
     "https://raw.githubusercontent.com/sabbir10s/jersey/main/public/Data.json";
 
@@ -27,10 +27,10 @@ const ProductDetails = () => {
     return "Loading...";
   }
   const singleProduct = products.find(
-    (product) => product.id === parseInt(productId)
+    (product) => product._id === parseInt(productId)
   );
   // console.log(products, "All product");
-  const { name, images, material, price } = singleProduct;
+  const { name, images, material, price, details } = singleProduct;
   console.log(singleProduct, "single product");
   return (
     <div className=" container mx-auto py-4">
@@ -38,39 +38,50 @@ const ProductDetails = () => {
         <div className="relative">
           <div className="absolute top-44 left-10 border border-primary w-52">
             <h3 className=" uppercase text-[64px] text-white text-center ">
-              {userName}
+              {jerseyName}
             </h3>
           </div>
           <div className="absolute top-36 right-10 border border-primary w-52">
             <h3 className=" uppercase text-[100px] text-white text-center ">
-              {number}
+              {jerseyNumber}
             </h3>
           </div>
           <img className="w-full object-fill" src={images.two} alt="" />
         </div>
         <div>
-          <h2>{name}</h2>
-          <p>Material: {material}</p>
-          <p>Price:{price}</p>
-          <form action="">
+          <div className=" space-y-1">
+            <h2 className="text-3xl font-semibold">{name}</h2>
+            <p>Material: {material}</p>
+            <p>{details}</p>
+          </div>
+          <p className="text-2xl text-primary font-semibold py-2">{price}</p>
+          <form className="max-w-[200px]" action="">
+            <label htmlFor="name">Name</label>
             <input
               required={true}
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-              className="px-4 py-3  rounded-lg w-full focus:border-primary outline-none border"
+              value={jerseyName}
+              onChange={(e) => setJerseyName(e.target.value)}
+              className="px-4 py-2 rounded w-full focus:border-primary outline-none border"
               type="text"
               placeholder="Name"
             />
+            <label htmlFor="number">Jersey Number</label>
             <input
               required={true}
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
-              className="px-4 py-3  rounded-lg w-full focus:border-primary outline-none border"
-              type="number"
-              placeholder="Number"
+              value={jerseyNumber}
+              onChange={(e) => setJerseyNumber(e.target.value)}
+              className="px-4 py-2 rounded w-full focus:border-primary outline-none border"
+              type="jerseyNumber"
+              placeholder="jerseyNumber"
             />
           </form>
-          <AddToCard product={singleProduct} />
+          <div>
+            <AddToCard
+              product={singleProduct}
+              jerseyName={jerseyName}
+              jerseyNumber={jerseyNumber}
+            />
+          </div>
         </div>
       </div>
     </div>
